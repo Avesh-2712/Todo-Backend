daeda05b00fac5ee38f4225e0b86c12e3f257106
Avesh-2712 <114075285+Avesh-2712@users.noreply.github.com>
2023-05-08 19:04:54 +0530

    Remove un necessory console.log

--- src/Config/dbConnection.js
+++ src/Config/dbConnection.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const { Model } = require('objection');
 const Knex = require('knex');
-console.log( process.env.DB_NAME);
 const knex = Knex({
     client: 'pg',
     connection: {
@@ -12,16 +11,7 @@ const knex = Knex({
   },
     debug: process.env.DB_DEBUG === 'true',
 })
-// console.log(knex);
 knex.on('query', data => {console.dir(data) });
-// knex.raw('SELECT 1 + 1 AS result')
-//   .then((result) => {
-//     console.log('Knex is connected to Postgres:', result.rows[0].result === 2);
-//   })
-//   .catch((err) => {
-//     console.log('Error connecting to Postgres:', err);
-//   });
-
 Model.knex(knex);
 
 module.exports = Model;
